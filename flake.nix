4e7e2d9ea60f9da5da8388ecd8b38592d822f24e
John Hampton <john.hampton@stanfordalumni.org>
2023-04-18 14:53:44 -0700

    Add terraform template

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
         path = ./nodeml;
         description = "A template for TAN NodeML development";
       };
+      terraform = {
+        path = ./terraform;
+        description = "A template for terraform development";
+      };
       typescript = {
         path = ./typescript;
         description = "A template for typescript development";
